adff17cf115cbecd5621d1305e951fced1ec664e
Vincent Tjia <vtjia51@gmail.com>
2022-07-07 18:28:03 -0400

    fixed navbar mobile issue

---
+++ index.js
@@ -0,0 +1,11 @@
+const burgerBtnEl = document.querySelector("#burger-btn");
+const menuEl = document.querySelector("#mobile-menu");
+
+burgerBtnEl.addEventListener('click', () => {
+    if(menuEl.classList.contains('hidden')) {
+        menuEl.classList.remove('hidden');
+    }
+    else {
+        menuEl.classList.add('hidden');
+    }
+})
